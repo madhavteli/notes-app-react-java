8d777b02f82829a55084015e73a13d8245b4f990
Madhav Teli <mteli@entrata.com>
2022-01-30 21:16:28 +0530

    base url error resolved

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
 	// baseURL: "http://localhost:8080/api",
-	baseURL: "https://notes-app-java.herokuapp.com/api",
+	baseURL: "https://notes-app-java-backend.herokuapp.com/api",
 	headers:{
 		"Content-type":"application/json"
 	}
